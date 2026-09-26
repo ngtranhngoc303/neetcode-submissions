728712a30beab89154da66641c4c9a3af22246e3
ngtranhngoc303 <nguyentruonganhngoc3003@gmail.com>
2026-09-25 22:09:49 -0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        int current = 1;
+        int previous = 1;
+        int next;
+        for(int i = 1; i < n; i++){
+            next = current + previous;
+            previous = current;
+            current = next;
+        }
+        return current;
+    }
+}
